4d74f605067500b068f61b4b17ed6e685dff2b41
Jiří <jirka.krucek@gmail.com>
2026-03-16 18:35:01 +0100

    Add SQL queries for customer revenue analysis
    Přidaní dalších pár syntaxí.

--- new_columns_sql.sql
+++ new_columns_sql.sql
@@ -52,3 +52,40 @@ SELECT * FROM order_items;
 
 
 
+SELECT c.customer_id,
+       SUM(oi.quantity * oi.unit_price_czk) AS revenue
+FROM customers AS c
+LEFT JOIN orders AS o
+    ON c.customer_id = o.customer_id
+JOIN order_items AS oi
+    ON o.order_id = oi.order_id
+WHERE o.order_datetime BETWEEN '2025-09-24' AND '2026-01-01' 
+		AND o.channel IN ('e-shop','store')
+GROUP BY c.customer_id
+HAVING SUM(oi.quantity * oi.unit_price_czk) > 200
+ORDER BY revenue
+LIMIT 5;
+
+
+
+SELECT 
+    c.customer_id,
+	c.email,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    SUM(oi.quantity * oi.unit_price_czk) AS revenue
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+JOIN order_items oi
+    ON o.order_id = oi.order_id
+WHERE o.order_revenue_czk > (
+        SELECT AVG(order_revenue_czk)
+        FROM orders
+)
+AND o.promo_code IN ('SPRING15', 'BF20')
+GROUP BY c.customer_id, c.first_name, c.last_name, c.email
+ORDER BY revenue DESC;
+
+
+
+
